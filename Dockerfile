a9a9a44ce58f0f56603ad8cc7724c214ada507a0
kembara <susiloharjo@gmail.com>
2021-03-31 12:28:30 +0700

    add postgres

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,10 @@ COPY app /app
 
 WORKDIR /app
 
+RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
-CMD [ "python", "main.py" ]
+# CMD [ "python", "main.py" ]
 
 
 
--- app/main.py
+++ app/main.py
@@ -8,5 +8,14 @@ def index():
     return jsonify(hello="world")
 
 
+@app.route('/nama/<name>')
+def name(name):
+    return jsonify(hello=name)
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+
+# sources
+# https://testdriven.io/blog/dockerizing-flask-with-postgres-gunicorn-and-nginx/
